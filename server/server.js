0d8bc6bf88fe2ba6fd50d834eaefc5b06c55ab88
Nikhil Sathe <sat.nikhil@gmail.com>
2020-10-10 09:57:23 +0530

    setup production build and server with heroku_update

--- server/server.js
+++ server/server.js
@@ -9,11 +9,11 @@ app.use(express.static(publicPath));
 
 //we can make a function to work when someone makes a get request to server. to match all unhandled requests or routes
 //processing all unhandled requests. send back the index.html files in public dir. req > contains info about requests. res>lets you manipulate the response to us exp server makes whoever made the https req. 
-app.get('*',(req,res) =>{
+app.get('*', (req,res) => {
      res.sendFile(path.join(publicPath,'index.html'));
 });
 
 //3000 is port number
-app.listen(3000,() => {
+app.listen(port,() => {
     console.log('Server is Up!');
 });
